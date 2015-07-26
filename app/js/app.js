dd56cad9be0890e433222334a5950295538f98ae
hoeck <eriksoehnel@googlemail.com>
2015-07-26 16:20:07 +0200

    Display the titles of the 5 topmost stories

--- app/js/app.js
+++ app/js/app.js
@@ -3,12 +3,18 @@
 var hnBrowser = angular.module('hnBrowser', ['firebase', 'hnBrowserControllers']),
     hnBrowserControllers = angular.module('hnBrowserControllers', []);
 
-hnBrowserControllers.controller('StoryListCtrl', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
+hnBrowserControllers.controller('StoryListCtrl', ['$scope', '$firebaseArray', '$firebaseObject', function ($scope, $firebaseArray, $firebaseObject) {
     var url = 'https://hacker-news.firebaseio.com/v0',
-        fireRef = new Firebase(url);
+        fireRef = new Firebase(url),
+        testStoryId = 9918365;
 
     // https://hacker-news.firebaseio.com/v0/topstories.json
     // https://hacker-news.firebaseio.com/v0/item/XXXX.json
-
-    $scope.topstories = $firebaseArray(fireRef.child('topstories'));
+    $scope.stories = [];
+    $scope.topstoryIds = $firebaseArray(fireRef.child('topstories'));
+    $scope.topstoryIds.$loaded().then(function () {
+        _.map(_.take($scope.topstoryIds, 5), function(x,i) {
+            $scope.stories[i] = $firebaseObject(fireRef.child('item').child(x.$value));
+        })
+    });
 }]);

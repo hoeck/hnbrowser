4d4aaf93de62cb6b88cda3134f6363243fc2b96d
hoeck <eriksoehnel@googlemail.com>
2015-07-28 07:41:43 +0200

    Add routing and implement a story detail view
    The story details show the level-0 comments horizontally.

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,26 @@
 'use strict';
 
-var hnBrowser = angular.module('hnBrowser', ['firebase', 'hnBrowserControllers']),
+var hnBrowser = angular.module('hnBrowser', ['firebase', 'hnBrowserControllers', 'ngRoute', 'ngSanitize']),
     hnBrowserControllers = angular.module('hnBrowserControllers', []);
+
+hnBrowser.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+            .when('/stories', {
+                template: function() {
+                    return document.getElementById('StoryListTmpl').innerText;
+                },
+                controller: 'StoryListCtrl',
+            })
+            .when('/stories/:itemId', {
+                template: function() {
+                    return document.getElementById('StoryTmpl').innerText;
+                },
+                controller: 'StoryCtrl',
+            })
+            .otherwise({
+                redirectTo: '/stories'
+            });
+}]);
 
 hnBrowserControllers.controller('StoryListCtrl', ['$scope', '$firebaseArray', '$firebaseObject', function ($scope, $firebaseArray, $firebaseObject) {
     var url = 'https://hacker-news.firebaseio.com/v0',
@@ -13,8 +32,22 @@ hnBrowserControllers.controller('StoryListCtrl', ['$scope', '$firebaseArray', '$
     $scope.stories = [];
     $scope.topstoryIds = $firebaseArray(fireRef.child('topstories'));
     $scope.topstoryIds.$loaded().then(function () {
-        _.map(_.take($scope.topstoryIds, 5), function(x,i) {
-            $scope.stories[i] = $firebaseObject(fireRef.child('item').child(x.$value));
+        $scope.stories = _.map(_.take($scope.topstoryIds, 5), function (x) {
+            return $firebaseObject(fireRef.child('item').child(x.$value));
         })
     });
+}]);
+
+hnBrowserControllers.controller('StoryCtrl', ['$scope', '$firebaseArray', '$firebaseObject', '$routeParams', function ($scope, $firebaseArray, $firebaseObject, $routeParams) {
+    var url = 'https://hacker-news.firebaseio.com/v0',
+        fireRef = new Firebase(url);
+
+    $scope.story = $firebaseObject(fireRef.child('item').child($routeParams.itemId));
+
+    $scope.kids = [];
+    $scope.story.$loaded().then(function () {
+        $scope.kids = _.map($scope.story.kids, function(x) {
+            return $firebaseObject(fireRef.child('item').child(x));
+        });
+    })
 }]);

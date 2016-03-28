3cd8f725ac60da6bad1e679c59996045198cb95b
Erik Söhnel <erik.soehnel@lovoo.com>
2016-03-28 08:49:18 +0200

    publish 4a22429124703bb4c165f9f2dde32076618084f2 to gh-pages

--- src/app/format.cljs
+++
@@ -1,8 +0,0 @@
-(ns app.format
-  (:require [goog.string :as gstring]
-            [goog.string.format]))
-
-(defn format
-  "Formats a string using goog.string.format."
-  [fmt & args]
-  (apply gstring/format fmt args))
